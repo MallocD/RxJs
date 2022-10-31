14520a9651b161715ed83c590048c4a5d299c09b
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-10-30 21:43:21 -0400

    Sincrono

--- js/promisse.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'rxjs.js';
-
-
-Promise.resolve(1)
- .then(num => console.log(num));
-
- create((observer) => {
-     observer.next(1);
- }).subscribe(num => console.log(num))
